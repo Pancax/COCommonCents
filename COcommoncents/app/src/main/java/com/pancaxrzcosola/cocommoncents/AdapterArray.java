06151b191180a907b35a489496cc3df52d971d1b
SolaEclipse <fishercbrayden@gmail.com>
2021-02-28 09:54:09 -0600

    Literally a Comment

--- COcommoncents/app/src/main/java/com/pancaxrzcosola/cocommoncents/AdapterArray.java
+++ COcommoncents/app/src/main/java/com/pancaxrzcosola/cocommoncents/AdapterArray.java
@@ -17,15 +17,33 @@ public class AdapterArray extends RecyclerView.Adapter<AdapterArray.ViewHolder> 
     ArrayList<PTransfer> obj;
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        private final TextView textView;
+        private final TextView transferAmtBox;
+        private final TextView transferIDBox;
+        private final TextView merchantBox;
+        private final TextView dateBox;
 
         public ViewHolder(View view) {
             super(view);
-            this.textView = (TextView) view.findViewById(R.id.transferAmtBox);
+            this.transferAmtBox = (TextView) view.findViewById(R.id.transferAmtBox);
+            this.transferIDBox = (TextView) view.findViewById(R.id.transferIDBox);
+            this.merchantBox = (TextView) view.findViewById(R.id.merchantBox);
+            this.dateBox = (TextView) view.findViewById(R.id.dateBox);
         }
 
-        public TextView getTextView(){
-            return textView;
+        public TextView getTransferAmtBox(){
+            return transferAmtBox;
+        }
+
+        public TextView getTransferIDBox() {
+            return transferIDBox;
+        }
+
+        public TextView getMerchantBox() {
+            return merchantBox;
+        }
+
+        public TextView getDateBox() {
+            return dateBox;
         }
     }
 
@@ -46,8 +64,12 @@ public class AdapterArray extends RecyclerView.Adapter<AdapterArray.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Log.d("TagString","Element "+position);
         try {
-            if(obj.get(position).purchase!=null)
-                holder.getTextView().setText("ID: "+obj.get(position).purchase.getString("_id")+" Amount: "+obj.get(position).purchase.getInt("amount"));
+            if(obj.get(position).purchase!=null) {
+                holder.getTransferAmtBox().setText(obj.get(position).transfer.getInt("amount"));
+                holder.getTransferIDBox().setText(obj.get(position).transfer.getString("_id"));
+                holder.getMerchantBox().setText(obj.get(position).purchase.getString("merchant_id"));
+                holder.getDateBox().setText(obj.get(position).transfer.getString("transaction_date"));
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
