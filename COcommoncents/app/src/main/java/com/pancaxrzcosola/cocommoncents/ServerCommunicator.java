ba1318d00ae54a790fdadcdb0c35760b140ec574
SolaEclipse <fishercbrayden@gmail.com>
2021-02-27 18:56:03 -0600

    Literally a Comment

--- COcommoncents/app/src/main/java/com/pancaxrzcosola/cocommoncents/ServerCommunicator.java
+++ COcommoncents/app/src/main/java/com/pancaxrzcosola/cocommoncents/ServerCommunicator.java
@@ -19,6 +19,7 @@ public class ServerCommunicator {
     private RequestQueue queue = null;
     final private String API_KEY ="37e93a473684febe1afbe773dbe31637";
     final private String CUSTOMER_ID_URL="http://api.nessieisreal.com/enterprise/customers/";//+id ad the end
+    final private String ACCOUNT_ID_URL="http://api.nessieisreal.com/enterprise/accounts/";//+id
 
 
     public ServerCommunicator(Context c){
@@ -28,6 +29,12 @@ public class ServerCommunicator {
     public void getCustomerForID(String id, Response.Listener<JSONObject> list, Response.ErrorListener eList){
         JSONObject send = new JSONObject();
         JsonObjectRequest request = new JsonObjectRequest(GET, CUSTOMER_ID_URL+id+"?key="+API_KEY, null, list,eList);
+        queue.add(request);
+    }
+
+    public void getAccountfromCustomerID(String id, Response.Listener<JSONObject> list, Response.ErrorListener eList){
+        JSONObject send = new JSONObject();
+        JsonObjectRequest request = new JsonObjectRequest(GET, ACCOUNT_ID_URL+id+"?key="+API_KEY,null,list,eList);
         queue.add(request);
     }
 
